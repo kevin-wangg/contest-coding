4551c015f12f5d880f5fabfc40d4f67e8fcfc3dc
Kevin Wang <kevinwang2040@gmail.com>
2018-03-12 23:38:55 -0400

    Update Pattern Generator.cpp

--- CCC/Pattern Generator.cpp
+++ CCC/Pattern Generator.cpp
@@ -1,59 +1,51 @@
+#include <vector>
 #include <iostream>
-#include <string>
+#include <algorithm>
+#define printv(a) for(int i = 0; i < a.size(); i++){cout << a.at(i) << " ";}cout << endl
 
 using namespace std;
 
+int n, k;
+vector<vector<int> > all;
+
+void recurse(vector<int> bit, int ones, int cur)
+{
+  /*for(int i = 0; i < bit.size(); i++){
+    cout << bit.at(i) << " ";
+  }
+  cout << endl;*/
+  
+  if(ones == k){
+    //cout << "wow" << endl;
+    all.push_back(bit);
+  }
+  else{
+    for(int i = cur; i < n; i++){
+      if(bit.at(i) == 0){
+        bit.at(i) = 1;
+        ones++;
+        recurse(bit, ones, i + 1);
+        bit.at(i) = 0;
+        ones--;
+      }
+    }
+  }
+}
+
 int main()
 {
-  long long t; cin >> t;
-  for(long long z = 0; z < t; z++){
-    string num; cin >> num;
-    string temp = num;
-    cout << num << endl;
-    while(num.size() > 2){
-      string subtract = "";
-      subtract += num.at(num.size() - 1);
-      long long sub = stoll(subtract);
-      
-      string subtractfrom = "";
-      long long count = 3;
-      while(true){
-        if(num.at(num.size() - count) != '0'){
-          break;
-        }
-        else{
-          count++;
-        }
+  int t; cin >> t;
+  for(int h = 0; h < t; h++){
+    cin >> n >> k;
+    all = {};
+    vector<int> bit(n, 0);
+    recurse(bit, 0, 0);
+    cout << "The bit patterns are" << endl;
+    for(int i = 0; i < all.size(); i++){
+      for(int j = 0; j < all.at(i).size(); j++){
+        cout << all.at(i).at(j);
       }
-      for(long long i = count; i > 1; i--){
-        subtractfrom += num.at(num.size() - i);
-      }
-      long long subfrom = stoll(subtractfrom);
-      long long newnum = subfrom - sub;
-      num.erase(num.size() - count, count);
-      if(to_string(newnum).size() > 1){
-        num += to_string(newnum);
-      }
-      else{
-        num += "0" + to_string(newnum);
-      }
-      if(num.size() == 2){
-        if(num.at(0) == '0'){
-          cout << num.at(1) << endl;
-        }
-        else{
-          cout << num << endl;
-        }
-      }
-      else{
-        cout << num << endl;
-      }
-    }
-    if(stoll(num) % 11 == 0){
-      cout << "The number " << temp << " is divisible by 11." << endl;
-    }
-    else{
-      cout << "The number " << temp << " is not divisible by 11." << endl;
+      cout << endl;
     }
     cout << endl;
   }
